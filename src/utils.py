14da3775a9ad0d51377809a2b50ad3b948ec0a5e
yash mohite <114924851+mohiteyashprogrammer@users.noreply.github.com>
2023-10-06 16:23:52 +0530

    utils Done

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,8 @@ from src.exception import CustomException
 from dotenv import load_dotenv
 import pymysql
 import pickle
+from sklearn.model_selection import GridSearchCV,RandomizedSearchCV
+from sklearn.metrics import r2_score,mean_squared_error,mean_absolute_error
 
 load_dotenv()
 # define variable
@@ -54,4 +56,51 @@ def save_object(filepath,obj):
 
     except Exception as e:
         raise(CustomException(e,sys))
+    
+
+def model_traning(X_train,y_train,X_test,y_test,models,param):
+
+    '''
+    This Function Will Train The model and Evaluate
+
+    '''
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            # model traning
+            grid = GridSearchCV(model,para,cv=3,n_jobs=-1)
+            grid.fit(X_train,y_train)
+
+            model.set_params(**grid.best_params_)
+            model.fit(X_train,y_train)
+
+            #make prediction
+            y_test_pred = model.predict(X_test)
+
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+
+def lode_object(file_path):
+    '''
+    This  Function Will Loab Pickel File And 
+    Read In binery Mode
+    
+    '''
+    try:
+        with open(file_path,"rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
 
